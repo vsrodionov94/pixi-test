f708dfc55aaaa2820ba7d27e0f6ff393fc1c6ecf
Vladimir <vsrodionov94@yandex.ru>
2021-11-03 13:26:20 +0700

    set new animation

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// const PIXI = require('./pixi');
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-let mouseX = width / 2;
-let mouseY = height / 2;
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
 
 let dX = 0;
 let dY = 0;
@@ -18,37 +13,58 @@ function onDocumentMouseMove({ clientX, clientY }) {
   mouseY = clientY;
 }
 
-
-const app = new PIXI.Application({ width: width, height: height });
+const app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
 document.body.appendChild(app.view);
-const sprite = PIXI.Sprite.from('./images/sphere-2.png');
-sprite.anchor.set(0.5);
-sprite.position.set(width / 2 - 150, height / 2 - 300);
-sprite.scale.set(0.5);
-app.stage.addChild(sprite);
+const width = app.screen.width;
+const height = app.screen.height;
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+const sprite1 = PIXI.Sprite.from('./images/sphere-2.png');
+sprite1.anchor.set(0.5);
+sprite1.position.set(width / 2 - 150, -150);
+sprite1.scale.set(0.5);
+app.stage.addChild(sprite1);
+container.addChild(sprite1);
 
 const sprite2 = PIXI.Sprite.from('./images/sphere-2.png');
 sprite2.anchor.set(0.5);
-sprite2.position.set(width / 2 + 300, height / 2 + 200);
+sprite2.position.set(width - 250, height / 2);
 sprite2.scale.set(0.5);
 app.stage.addChild(sprite2);
+container.addChild(sprite2);
 
 const sprite3 = PIXI.Sprite.from('./images/sphere-3.png');
 sprite3.anchor.set(0.5);
 sprite3.scale.set(0.7);
-sprite3.position.set(0 - 100, height + 200);
-
+sprite3.position.set(200, height - 20);
 app.stage.addChild(sprite3);
+container.addChild(sprite3);
+
+// const displacementSprite = PIXI.Sprite.from('./images/displace.png');
+// const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+// app.stage.addChild(displacementSprite);
+// container.filters = [displacementFilter];
+// displacementFilter.scale.x = 110;
+// displacementFilter.scale.y = 110;
+// displacementSprite.anchor.set(0.5);
+
+
+window.onresize = () => {
+  app.resizeTo = window;
+  app.resize();
+  // console.log(app);
+};
 
 app.ticker.add((delta) => {
-  sprite.x -= dX / 10;
-  sprite.y -= dY / 10;
-  sprite2.x -= dX / 20;
-  sprite2.y -= dY / 20;
-  sprite3.x -= dX / 30;
-  sprite3.y -= dY / 30;
+  // displacementSprite.position.set(mouseX, mouseY);
+  sprite1.x += dX / 8;
+  sprite1.y += dY / 8;
+  sprite2.x -= dX / 8;
+  sprite2.y -= dY / 8;
+  sprite3.x += dX / 7;
+  sprite3.y -= dY / 7;
   dX = 0;
   dY = 0;
-})
-
-console.log(sprite)
+});
