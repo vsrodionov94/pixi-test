15985515e92f358f0db53dd17a26815cab9188dc
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-02 16:51:04 +0700

    add init points

--- index.js
+++ index.js
@@ -1,33 +1,85 @@
-const createSphere = (key, radius, x, y) => {
-  const points = [];
-
-  // for (let i = 0; i <= 360; i += 30) {
-  //   const x = Math.round(radius * Math.cos(i * Math.PI / 180));
-  //   const y = Math.round(radius * Math.sin(i * Math.PI / 180));
-  //   points.push(new PIXI.Point(x, y));
-  // }
-
-  const dX = 20
-  for (let i = 0; i <= radius * 2; i += dX) {
-    points.push(new PIXI.Point(i, y));
+class Spere {
+  constructor(key, radius, x, y) {
+    this.key = key;
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.dX = 20;
+    this.points = [];
+    this.initPoints();
+    this.initStrip();
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.speed = 10;
+    this.targetX = 0;
+    this.targetY = 0;
+    this.speed = 10;
+    this.offset = 100;
   }
 
-  const strip = new PIXI.SimpleRope(PIXI.Texture.from(`images/${key}.png`), points);
-  strip.x = x;
-  strip.y = y;
-
-  const movementX = (dX) => {
-    for (let i = 0; i < points.length; i += 1) {
-      points[i].x += dX;
+  initPoints() {
+    for (let i = 0; i <= this.radius * 2; i += this.dX) {
+      this.points.push(new PIXI.Point(i, this.y));
     }
+  }
+
+  initStrip() {
+    this.strip = new PIXI.SimpleRope(PIXI.Texture.from(`images/${this.key}.png`), this.points);
+    this.strip.x = this.x;
+    this.strip.y = this.y;
+  }
+
+  incVelosityX(x) {
+    this.velocityX += x;
+  }
+
+  incVelosityX(y) {
+    this.velocityY += y;
+  }
+
+  update(delta, mouseX, mouseY) {
+    // if (mouseX !== this.targetX) {
+      this.targetX = mouseX;
+      if (Math.abs(this.x - this.targetX) > this.offset) {
+        const dX = Math.sign(this.targetX - this.x) * this.speed * delta;
+        this.movementX(dX);
+      }
+    // }
+    // if (mouseY !== this.targetY) {
+      this.targetY = mouseY;
+      if (Math.abs(this.y - this.targetY) > this.offset) {
+        const dY = Math.sign(this.targetY - this.y) * this.speed * delta;
+        this.movementY(dY);
+      }
+    // }
+  }
+
+  movementX(dX) {
+    this.x += dX;
+    if (dX > 0) {
+      for (let i = 0; i < this.points.length; i += 1) {
+        this.points[i].x += dX * (i / 10);
+      }
+    } else {
+      for (let i = 0; i < this.points.length; i += 1) {
+        this.points[i].x += dX * ((this.points.length - i) / 10);
+      }
+    }
+    console.log(dX)
   };
 
-  const movementY = (dY) => {
-    for (let i = 0; i < points.length; i += 1) {
-      points[i].y += dY;
+  movementY(dY) {
+    this.y += dY;
+    if (dY > 0) {
+      for (let i = 0; i < this.points.length; i += 1) {
+        this.points[i].y += dY * (i / 10);
+      }
+    } else {
+      for (let i = 0; i < this.points.length; i += 1) {
+        this.points[i].y += dY * ((this.points.length - i) / 10);
+      }
     }
   }
-  return { points, strip, movementX, movementY };
 };
 
 // const PIXI = require('./pixi');
@@ -44,7 +96,7 @@ let dY = 0;
 document.addEventListener( 'mousemove', onDocumentMouseMove );
 
 function onDocumentMouseMove({ clientX, clientY }) {
-  dX = mouseX - clientX;
+  dY = mouseX - clientX;
   dY = mouseY - clientY;
   mouseX = clientX;
   mouseY = clientY;
@@ -54,19 +106,20 @@ const app = new PIXI.Application({ width: width, height: height });
 document.body.appendChild(app.view);
 
 
-const sphere1 = createSphere('sphere-2', 800, 200, 300);
+const sphere1 = new Spere('sphere-2', 800, 200, 300);
 app.stage.addChild(sphere1.strip);
-const sphere2 = createSphere('sphere-2', 800, width / 2 + 300, height / 2 + 200);
+const sphere2 = new Spere('sphere-2', 800, width / 2 + 300, height / 2 + 200);
 app.stage.addChild(sphere2.strip);
-const sphere3 = createSphere('sphere-3', 800, 0 - 100, height + 200);
+const sphere3 = new Spere('sphere-3', 800, 0 - 100, height + 200);
 app.stage.addChild(sphere3.strip);
 console.log(sphere1)
 console.log(sphere2)
 console.log(sphere3)
 
 app.ticker.add((delta) => {
-  sphere1.movementX(dX / 10);
-  sphere1.movementY(dY / 10);
+  sphere1.update(delta, mouseX, mouseY);
+  // sphere1.movementX(-dX / 10);
+  // sphere1.movementY(-dY / 10);
   // sprite2.x -= dX / 20;
   // sprite2.y -= dY / 20;
   // sprite3.x -= dX / 30;
