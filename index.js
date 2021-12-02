39daa87131e4090e47eb6ac77409c1dc13ee06e7
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-02 14:28:24 +0700

    add movement

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const createSphere = (key, radius, x, y) => {
   const points = [];
 
-  for (let i = 0; i <= 360; i += 30) {
-    const x = Math.round(radius * Math.cos(i * Math.PI / 180));
-    const y = Math.round(radius * Math.sin(i * Math.PI / 180));
-    points.push(new PIXI.Point(x, y));
+  // for (let i = 0; i <= 360; i += 30) {
+  //   const x = Math.round(radius * Math.cos(i * Math.PI / 180));
+  //   const y = Math.round(radius * Math.sin(i * Math.PI / 180));
+  //   points.push(new PIXI.Point(x, y));
+  // }
+
+  const dX = 20
+  for (let i = 0; i <= radius * 2; i += dX) {
+    points.push(new PIXI.Point(i, y));
   }
 
   const strip = new PIXI.SimpleRope(PIXI.Texture.from(`images/${key}.png`), points);
   strip.x = x;
   strip.y = y;
-  return { points, strip };
+
+  const movementX = (dX) => {
+    for (let i = 0; i < points.length; i += 1) {
+      points[i].x += dX;
+    }
+  };
+
+  const movementY = (dY) => {
+    for (let i = 0; i < points.length; i += 1) {
+      points[i].y += dY;
+    }
+  }
+  return { points, strip, movementX, movementY };
 };
 
 // const PIXI = require('./pixi');
@@ -37,37 +54,19 @@ const app = new PIXI.Application({ width: width, height: height });
 document.body.appendChild(app.view);
 
 
-const sphere1 = createSphere('sphere-2', 200, 200, 300);
+const sphere1 = createSphere('sphere-2', 800, 200, 300);
 app.stage.addChild(sphere1.strip);
-const sphere2 = createSphere('sphere-2', 200, width / 2 + 300, height / 2 + 200);
+const sphere2 = createSphere('sphere-2', 800, width / 2 + 300, height / 2 + 200);
 app.stage.addChild(sphere2.strip);
-const sphere3 = createSphere('sphere-3', 200, 0 - 100, height + 200);
+const sphere3 = createSphere('sphere-3', 800, 0 - 100, height + 200);
 app.stage.addChild(sphere3.strip);
 console.log(sphere1)
 console.log(sphere2)
 console.log(sphere3)
-// const sprite = PIXI.Sprite.from('./images/sphere-2.png');
-// sprite.anchor.set(0.5);
-// sprite.position.set(width / 2 - 150, height / 2 - 300);
-// sprite.scale.set(0.5);
-// app.stage.addChild(sprite);
-
-// const sprite2 = PIXI.Sprite.from('./images/sphere-2.png');
-// sprite2.anchor.set(0.5);
-// sprite2.position.set(width / 2 + 300, height / 2 + 200);
-// sprite2.scale.set(0.6);
-// app.stage.addChild(sprite2);
-
-// const sprite3 = PIXI.Sprite.from('./images/sphere-3.png');
-// sprite3.anchor.set(0.5);
-// sprite3.scale.set(0.7);
-// sprite3.position.set(0 - 100, height + 200);
-
-// app.stage.addChild(sprite3);
 
 app.ticker.add((delta) => {
-  // sprite.x -= dX / 10;
-  // sprite.y -= dY / 10;
+  sphere1.movementX(dX / 10);
+  sphere1.movementY(dY / 10);
   // sprite2.x -= dX / 20;
   // sprite2.y -= dY / 20;
   // sprite3.x -= dX / 30;
